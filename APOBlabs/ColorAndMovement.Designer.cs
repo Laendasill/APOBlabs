88a1eb8538b69d69e7d791380fb235b2e857bf06
laendasill <panador@gmail.com>
2015-06-28 13:51:15 +0200

    project

---
+++ APOBlabs/ColorAndMovement.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace APOBlabs
+{
+    partial class ColorAndMovement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ImagesLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.Colormode = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addImagesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(393, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addImagesToolStripMenuItem
+            // 
+            this.addImagesToolStripMenuItem.Name = "addImagesToolStripMenuItem";
+            this.addImagesToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.addImagesToolStripMenuItem.Text = "AddImages";
+            this.addImagesToolStripMenuItem.Click += new System.EventHandler(this.addImagesToolStripMenuItem_Click);
+            // 
+            // ImagesLayout
+            // 
+            this.ImagesLayout.AutoScroll = true;
+            this.ImagesLayout.AutoSize = true;
+            this.ImagesLayout.ColumnCount = 1;
+            this.ImagesLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ImagesLayout.Location = new System.Drawing.Point(0, 27);
+            this.ImagesLayout.Name = "ImagesLayout";
+            this.ImagesLayout.RowCount = 1;
+            this.ImagesLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ImagesLayout.Size = new System.Drawing.Size(86, 23);
+            this.ImagesLayout.TabIndex = 1;
+            // 
+            // Colormode
+            // 
+            this.Colormode.AutoSize = true;
+            this.Colormode.Location = new System.Drawing.Point(297, 33);
+            this.Colormode.Name = "Colormode";
+            this.Colormode.Size = new System.Drawing.Size(84, 17);
+            this.Colormode.TabIndex = 0;
+            this.Colormode.Text = "Kolorowanie";
+            this.Colormode.UseVisualStyleBackColor = true;
+            this.Colormode.CheckedChanged += new System.EventHandler(this.Colormode_CheckedChanged);
+            // 
+            // ColorAndMovement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(393, 455);
+            this.Controls.Add(this.Colormode);
+            this.Controls.Add(this.ImagesLayout);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ColorAndMovement";
+            this.Text = "ColorAndMovement";
+            this.Load += new System.EventHandler(this.ColorAndMovement_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addImagesToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel ImagesLayout;
+        private System.Windows.Forms.CheckBox Colormode;
+    }
+}
---
+++ APOBlabs/ColorAndMovement.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageMagick.Drawables;
+namespace APOBlabs
+{
+    public partial class ColorAndMovement : Form
+    {
+        PictureBox[] arrayOfImages;
+        int item = 0;
+        int maxitem = 0;
+        bool ChangeColorMode = false;
+        public ColorAndMovement()
+        {
+            InitializeComponent();
+            ImagesLayout.ColumnCount = 0;
+            ImagesLayout.RowCount = 0;
+            ImagesLayout.AutoScroll = false;
+            Show();
+        }
+
+        private void addImagesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog FileOpen = new OpenFileDialog();
+            FileOpen.Title = "Open Image File";
+            FileOpen.Multiselect = true;
+            FileOpen.Filter = "JPEG (*.jpg)|*.jpg|Bitmap (*.bmp)|*.bmp|TIFF (*.tiff)|*.tiff|All (*.*)|*.*";
+            DialogResult Result = FileOpen.ShowDialog();
+            if (Result == DialogResult.OK)
+            {
+                //ImageWindow img = new ImageWindow(this, FileOpen.FileName);
+                arrayOfImages = new PictureBox[FileOpen.FileNames.Length];
+                int i = 0;
+                maxitem = FileOpen.FileNames.Length;
+                String y = "";
+                foreach (String image in FileOpen.FileNames)
+                {
+                    try
+                    {
+                            y += image + "  ";
+                            PictureBox pb = new PictureBox();
+                            Image loadedImage = Image.FromFile(image);
+                            pb.Height = loadedImage.Height;
+                            pb.Width = loadedImage.Width;
+                            pb.Image = loadedImage;
+                            pb.MouseClick += pb_MouseClick;
+                            arrayOfImages[i] = pb;
+                           // ImagesLayout.Width = loadedImage.Width;
+                           // ImagesLayout.Height = loadedImage.Height;
+                           
+                            i += 1;
+                           
+                    
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show("Cannot display the image: " + image.Substring(image.LastIndexOf('\\'))
+                        + ". You may not have permission to read the file, or " +
+                        "it may be corrupt.\n\nReported error: " + ex.Message);
+                    }
+                }
+                UpdateImages();
+            }
+            else FileOpen.Dispose();
+        }
+
+        void pb_MouseClick(object sender, MouseEventArgs e)
+        {
+            PictureBox pom = (PictureBox)sender;
+            Bitmap pomm = (Bitmap)pom.Image;
+            
+            Color pixel  = pomm.GetPixel(e.X,e.Y);
+            if (ChangeColorMode == true)
+            {
+                ChangeColor(pom, pixel);
+            }
+            else
+            {
+                MessageBox.Show(e.X.ToString() + " | " + e.Y.ToString() + "=" + pixel.ToString());
+            }
+
+        }
+
+
+
+        private void ColorAndMovement_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateImages()
+        {
+            foreach (PictureBox i in arrayOfImages)
+            {
+                ImagesLayout.Controls.Add(i);
+            }
+        }
+
+        private void Colormode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChangeColorMode == false)
+            {
+                ChangeColorMode = true;
+            }
+            else
+            {
+                ChangeColorMode = false;
+            }
+        }
+
+        private void ChangeColor(PictureBox image, Color color)
+        {
+            Bitmap pomm = (Bitmap)image.Image;
+            for (int y = 0; y < pomm.Height; y += 1)
+            {
+                for (int x = 0; x < pomm.Width; x += 1)
+                {
+                    Color c = pomm.GetPixel(x, y);
+                    if (c != color)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        pomm.SetPixel(x, y, GetChosedColor());
+                    }
+                }
+            }
+
+            image.Image = pomm;
+        }
+
+        private Color GetChosedColor()
+        {
+            Color c = Color.Blue;
+
+            return c;
+        }
+
+        
+
+    }
+}
